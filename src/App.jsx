314cae0df8e84d8c6248f1341b8f5dc24980eaff
tasi <taegyung.e@gmail.com>
2025-05-31 18:16:40 +0900

    chore: App.jsx와 main.jsx에 import되어 있는 App.css, index.css 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 
-import FeedPage from './pages/FeedPage';
+import FeedPage from './pages/FeedPage/FeedPage';
 import { GlobalStyle } from './styles/GlobalStyle';
-import './App.css';
 
 function App() {
   return (
     <>
-      <GlobalStyle>
-        <Routes>
-          <Route path='/post/:id' element={<FeedPage />} />
-        </Routes>
-      </GlobalStyle>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/post/:id' element={<FeedPage />} />
+      </Routes>
     </>
   );
 }
