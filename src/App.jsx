c01f08aa9554f2df5762963cabec922b467eadeb
김성주 <aaa8757k@gmail.com>
2025-06-01 15:53:36 +0900

    chore: 초기세팅 수정
    -favi.ico, og추가, title변경
    -GlobalStyle.jsx 내용 추가
    -vite.config alias 설정
    -api.js추가
    -App.css, index.css  삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
+import FeedPage from '@pages/FeedPage';
+import { GlobalStyle } from '@styles/GlobalStyle';
 import { Routes, Route } from 'react-router-dom';
-
-import FeedPage from './pages/FeedPage';
-import { GlobalStyle } from './styles/GlobalStyle';
-import './App.css';
 
 function App() {
   return (
     <>
-      <GlobalStyle>
-        <Routes>
-          <Route path='/post/:id' element={<FeedPage />} />
-        </Routes>
-      </GlobalStyle>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/post/:id' element={<FeedPage />} />
+      </Routes>
     </>
   );
 }
