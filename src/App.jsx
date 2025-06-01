aeb78ed18250eca1ae843c00f9c7dbc82345b528
Squarecat-meow <Yozumina@serafuku.moe>
2025-06-01 23:07:53 +0900

    chore(project): 변경된 프로젝트 재 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
 import FeedPage from './pages/FeedPage';
-import { GlobalStyle } from './styles/GlobalStyle';
 import './App.css';
 
 function App() {
   return (
     <>
-      <GlobalStyle>
-        <Routes>
-          <Route path='/post/:id' element={<FeedPage />} />
-        </Routes>
-      </GlobalStyle>
+      <Routes>
+        <Route path='/post/:id' element={<FeedPage />} />
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App.jsx';
+import { GlobalStyle } from './styles/GlobalStyle.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </StrictMode>,
