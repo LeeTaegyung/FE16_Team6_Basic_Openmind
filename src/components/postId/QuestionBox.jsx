cc324386cf7dfaee40349addc25e415e2ae23c15
Squarecat-meow <Yozumina@serafuku.moe>
2025-06-02 11:46:11 +0900

    feat(components): QuestionBox 컴포넌트 추가

---
+++ src/components/postId/QuestionBox.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+function QuestionBox({ children }) {
+  return (
+    <QuestionBoxWrapper>
+      <div>
+        <QuestionBoxText>질문 · </QuestionBoxText>
+        <QuestionBoxCreatedAt>{children.createdAt}</QuestionBoxCreatedAt>
+      </div>
+      <QuestionBoxContent>{children.content}</QuestionBoxContent>
+    </QuestionBoxWrapper>
+  );
+}
+
+export default QuestionBox;
+
+const QuestionBoxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+`;
+
+const QuestionBoxText = styled.span`
+  color: var(--gray-40);
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const QuestionBoxCreatedAt = styled.span`
+  color: var(--gray-40);
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const QuestionBoxContent = styled.span``;
