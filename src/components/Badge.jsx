2d332558a5cc70e094ef1b27570b694be41f77fc
Squarecat-meow <Yozumina@serafuku.moe>
2025-06-02 09:16:06 +0900

    refactor(Badge): Badge 컴포넌트 리팩토링

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,35 +1,26 @@
 import styled from 'styled-components';
 
-const answerStatus = Object.freeze({
-  NOT_ANSWERED: 'notAnswered',
-  ANSWERED: 'answered',
-});
-
-function Badge({ status }) {
-  switch (status) {
-    case answerStatus.NOT_ANSWERED:
-      return <NotAnsweredBadge>미답변</NotAnsweredBadge>;
-
-    case answerStatus.ANSWERED:
-      return <AnsweredBadge>답변 완료</AnsweredBadge>;
-
-    default:
-      console.warn(`Badge Component: Unknown status: ${status}`);
-      return;
-  }
+function Badge({ variant, children }) {
+  return <StyledBadge variant={variant}>{children}</StyledBadge>;
 }
 
 export default Badge;
 
-const AnsweredBadge = styled.div`
+const StyledBadge = styled.div`
+  width: fit-content;
   padding: 4px 12px;
-  border: 1px solid #542f1a;
   border-radius: 8px;
-`;
+  font-size: 14px;
 
-const NotAnsweredBadge = styled.div`
-  padding: 4px 12px;
-  border: 1px solid #818181;
-  color: #818181;
-  border-radius: 8px;
+  ${(props) =>
+    props.variant === 'notAnswered' &&
+    `border: 1px solid var(--gray-40);
+    color: var(--gray-40);
+  `}
+
+  ${(props) =>
+    props.variant === 'answered' &&
+    `border: 1px solid var(--brown-40);
+    color: var(--brown-40);
+  `}
 `;
