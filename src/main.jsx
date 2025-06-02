f3145cb3d0d9cd6a7d1ec0e1e33354a9e3ccde9b
Squarecat-meow <Yozumina@serafuku.moe>
2025-06-02 18:56:22 +0900

    chore(git): merge conflict 수정

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,19 @@ import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import App from './App.jsx';
 import { GlobalStyle } from './styles/GlobalStyle.jsx';
+import { theme } from './styles/theme.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GlobalStyle />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 );
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        replaceAttrValues: {
+          '#000': 'currentColor',
+          '#000000': 'currentColor',
+          black: 'currentColor',
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@context': path.resolve(__dirname, 'src/context'),
