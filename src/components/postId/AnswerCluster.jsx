33ef0e5f2f42fe57d78639c31d9503b1149fe3af
Squarecat-meow <Yozumina@serafuku.moe>
2025-06-04 14:01:11 +0900

    feat(AnswerCluster): 답변이 없으면 아무것도 없다고 표시

--- src/components/postId/AnswerCluster.jsx
+++ src/components/postId/AnswerCluster.jsx
@@ -1,26 +1,62 @@
 import styled from 'styled-components';
 
 import AnswerItem from './AnswerItem';
+import Empty from '../../assets/Empty.svg?react';
 import Message from '../../assets/Messages.svg?react';
 
 function AnswerCluster({ subjectInfo, result }) {
   return (
-    <AnswerClusterWrapper>
-      <AnswerClusterText>
-        <Message height={24} />
-        {result.length}개의 질문이 있습니다.
-      </AnswerClusterText>
-      {result.map((el) => (
-        <AnswerItem key={el.id} subjectInfo={subjectInfo} result={el} />
-      ))}
-    </AnswerClusterWrapper>
+    <AnswerClusterBody>
+      <AnswerClusterWrapper>
+        {result ? (
+          <>
+            <AnswerClusterText>
+              <Message height={24} />
+              {result.count !== 0 ? (
+                <>{result.count}개의 질문이 있습니다.</>
+              ) : (
+                <>아직 질문이 없습니다.</>
+              )}
+            </AnswerClusterText>
+            {result.results.length !== 0 ? (
+              <>
+                {result.results &&
+                  result.results.map((el) => (
+                    <AnswerItem
+                      key={el.id}
+                      subjectInfo={subjectInfo}
+                      result={el}
+                    />
+                  ))}
+              </>
+            ) : (
+              <AnswerClusterEmpty>
+                <Empty />
+              </AnswerClusterEmpty>
+            )}
+          </>
+        ) : (
+          <>
+            <AnswerClusterText>로딩중...</AnswerClusterText>
+          </>
+        )}
+      </AnswerClusterWrapper>
+    </AnswerClusterBody>
   );
 }
 
 export default AnswerCluster;
 
+const AnswerClusterBody = styled.main`
+  width: 100%;
+  margin-top: 48px;
+  padding: 0px 24px;
+  display: flex;
+  justify-content: center;
+`;
+
 const AnswerClusterWrapper = styled.div`
-  max-width: 716px;
+  width: 716px;
   padding: 16px;
   display: flex;
   flex-direction: column;
@@ -38,4 +74,11 @@ const AnswerClusterText = styled.span`
   gap: 8px;
   color: ${({ theme }) => theme.color.brown40};
   font-size: 20px;
+`;
+
+const AnswerClusterEmpty = styled.div`
+  width: 100%;
+  padding: 16px 0px;
+  display: flex;
+  justify-content: center;
 `;
