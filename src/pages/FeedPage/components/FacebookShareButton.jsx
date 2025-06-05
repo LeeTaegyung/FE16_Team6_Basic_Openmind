cc0fe070fc2a269bab47fc1d5f22b017201801b2
tasi <taegyung.e@gmail.com>
2025-06-05 17:40:16 +0900

    feat(facebookShare): 페이스북 공유하기 기능 추가

--- src/pages/FeedPage/components/FacebookShareButton.jsx
+++ src/pages/FeedPage/components/FacebookShareButton.jsx
@@ -1,15 +1,19 @@
-import IconFacebookShare from '@assets/icons/IconFacebookShare.svg';
+import IconFacebookShare from '@assets/icons/IconFacebookShare.svg?react';
+import { Link } from 'react-router-dom';
 
 function FacebookShareButton() {
+  const ShareURL = `https://www.facebook.com/sharer/sharer.php?u=${location.href}`;
+
   return (
-    <button>
-      <img
-        src={IconFacebookShare}
-        alt='페이스북 공유 아이콘'
-        width='40'
-        height='40'
-      />
-    </button>
+    <>
+      <Link to={ShareURL} target='_blank'>
+        <IconFacebookShare
+          width='40'
+          height='40'
+          aria-label='페이스북 공유 아이콘'
+        />
+      </Link>
+    </>
   );
 }
 
