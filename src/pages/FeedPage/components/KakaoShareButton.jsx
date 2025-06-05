0b17908a01dcfd36b4d2ea958c6daeafdfa82764
tasi <taegyung.e@gmail.com>
2025-06-05 15:35:59 +0900

    feat(kakaoShare): 카카오톡 공유하기 기능 구현

--- src/pages/FeedPage/components/KakaoShareButton.jsx
+++ src/pages/FeedPage/components/KakaoShareButton.jsx
@@ -1,13 +1,34 @@
-import IconKakaoShare from '@assets/icons/IconKakaoShare.svg';
+import { useEffect } from 'react';
 
-function KakaoShareButton() {
+import IconKakaoShare from '@assets/icons/IconKakaoShare.svg?react';
+
+const { Kakao } = window;
+
+function KakaoShareButton({ name, subjectId }) {
+  const KAKAO_APP_KEY = import.meta.env.VITE_KAKAO_APP_KEY;
+
+  const handleKakaoShare = () => {
+    Kakao.Share.sendCustom({
+      templateId: 121287, // 카카오톡 개발센터에서 설정한 template 설정
+      templateArgs: {
+        userName: name,
+        questionId: subjectId,
+      },
+    });
+  };
+
+  useEffect(() => {
+    Kakao.cleanup();
+    Kakao.init(KAKAO_APP_KEY);
+    console.log(Kakao.isInitialized());
+  }, []);
+
   return (
-    <button>
-      <img
-        src={IconKakaoShare}
-        alt='카카오톡 공유 아이콘'
+    <button onClick={handleKakaoShare}>
+      <IconKakaoShare
         width='40'
         height='40'
+        aria-label='카카오톡 공유 아이콘'
       />
     </button>
   );
