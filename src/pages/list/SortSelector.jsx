95996903bf201e83efb3ba0c0247b67ed3531750
김성주 <aaa8757k@gmail.com>
2025-06-08 15:35:45 +0900

    feat(list): 정렬 셀렉터 기능 추가+추상화

--- src/pages/list/SortSelector.jsx
+++ src/pages/list/SortSelector.jsx
@@ -1,35 +1,53 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 
 import ArrowDown from '@assets/icons/ArrowDown.svg?react';
 import ArrowUp from '@assets/icons/ArrowUp.svg?react';
-import useClickOutside from '@hooks/useClickOutside';
-import styled from 'styled-components';
+import Selector from '@components/Selector';
+import { useClickOutside } from '@hooks/useClickOutside';
 
-const sortObj = {
+const sortMenu = {
   time: '최신순',
   name: '이름순',
 };
 
-function SortSelector() {
-  // 나중에 쿼리스트링으로 편입
-  const [order, setOrder] = useState('time');
-  const selectorRef = useRef();
-
+function SortSelector({ queryStrings, setQueryStrings }) {
+  const { sort } = queryStrings;
+  const selectorRef = useRef(null);
   const [isOpen, handleToggle] = useClickOutside(selectorRef);
 
+  //쿼리스트링 변경하기
   function handleClick(key) {
-    setOrder(key);
+    setQueryStrings((prev) => {
+      return {
+        ...prev,
+        sort: `${key}`,
+      };
+    });
+  }
+
+  function renderMenu() {
+    return Object.keys(sortMenu).map((key) => {
+      return (
+        <li
+          onClick={() => handleClick(key)}
+          key={key}
+          className={key === sort ? 'selected' : null}
+        >
+          {sortMenu[key]}
+        </li>
+      );
+    });
   }
 
   return (
     <Selector
       ref={selectorRef}
-      onClick={handleToggle}
-      className={isOpen ? 'isActive' : null}
-      aria-label='정렬 순서 선택'
+      isOpen={isOpen}
+      onToggle={handleToggle}
+      renderMenu={renderMenu}
     >
-      <button className={`sortBtn ${isOpen ? 'isActive' : null}`}>
-        {sortObj[order]}
+      <button className={`selectorBtn ${isOpen ? 'isActive' : null}`}>
+        {sortMenu[sort]}
         <ArrowUp
           className={!isOpen ? 'show' : null}
           aria-label='정렬 드롭다운 올리기'
@@ -39,94 +57,8 @@ function SortSelector() {
           aria-label='정렬 드롭다운 내리기'
         />
       </button>
-      {isOpen && (
-        <SortUl>
-          {Object.keys(sortObj).map((key) => {
-            return (
-              <li
-                onClick={() => handleClick(key)}
-                key={key}
-                className={key === order ? 'selected' : null}
-              >
-                {sortObj[key]}
-              </li>
-            );
-          })}
-        </SortUl>
-      )}
     </Selector>
   );
 }
 
 export default SortSelector;
-
-// 커스텀 셀렉터
-const Selector = styled.div`
-  position: relative;
-
-  .sortBtn {
-    padding: 8px 12px;
-    border-radius: 8px;
-    display: flex;
-    font-size: ${({ theme }) => theme.fontSize.fz14};
-    line-height: ${({ theme }) => theme.lineHeight.lh18};
-    gap: 4px;
-    align-items: center;
-    font-weight: 500;
-
-    border: 1px solid ${({ theme }) => theme.color.gray40};
-    background-color: ${({ theme }) => theme.color.gray10};
-    color: ${({ theme }) => theme.color.gray40};
-
-    &.isActive {
-      border-color: ${({ theme }) => theme.color.gray60};
-      color: ${({ theme }) => theme.color.gray60};
-    }
-  }
-
-  svg {
-    width: 14px;
-    height: 14px;
-  }
-
-  .show {
-    display: none;
-  }
-`;
-
-const SortUl = styled.ul`
-  position: absolute;
-  top: 100%;
-  left: 0;
-  border-radius: 8px;
-  box-shadow: var(--shadow-1);
-  width: 100%;
-  margin-top: 4px;
-
-  & li {
-    cursor: pointer;
-    font-size: ${({ theme }) => theme.fontSize.fz14};
-    line-height: ${({ theme }) => theme.lineHeight.lh18};
-    padding: 6px 16px;
-    color: ${({ theme }) => theme.color.gray50};
-    border: 1px solid ${({ theme }) => theme.color.gray30};
-    border-radius: 8px;
-    background-color: ${({ theme }) => theme.color.gray10};
-
-    &:first-child {
-      border-bottom: none;
-      border-bottom-left-radius: 0;
-      border-bottom-right-radius: 0;
-    }
-
-    &:nth-child(2) {
-      border-top: none;
-      border-top-right-radius: 0;
-      border-top-left-radius: 0;
-    }
-
-    &.selected {
-      color: ${({ theme }) => theme.color.blue50};
-    }
-  }
-`;
