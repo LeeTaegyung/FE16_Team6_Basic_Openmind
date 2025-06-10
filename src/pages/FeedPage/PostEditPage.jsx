5921d6989118e8913f1dd5ed0f3d983ae75db1a6
tasi <taegyung.e@gmail.com>
2025-06-11 01:41:08 +0900

    chore(PostEditPage): 코드 수정

--- src/pages/FeedPage/PostEditPage.jsx
+++ src/pages/FeedPage/PostEditPage.jsx
@@ -4,11 +4,12 @@ import AnswerCluster from './components/AnswerCluster';
 
 function PostEditPage() {
   const currentId = localStorage.getItem('subjectId') ?? ''; // getItem에 들어가는 subjectId도 userId로 명칭을 정정해주는 것이 좋아 보임.
-  const { id: subjectId } = useParams();
+  const { id: userId } = useParams();
+  const isEditable = currentId === userId;
 
   return (
     <>
-      <AnswerCluster isEditable={currentId === subjectId} />
+      <AnswerCluster isEditable={isEditable} />
     </>
   );
 }
