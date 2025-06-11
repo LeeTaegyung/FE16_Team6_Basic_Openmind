bd1a1416ea69b91799c49f0d7fcca58944eac10a
Apologle <otl5994@naver.com>
2025-06-11 14:48:32 +0900

    이름 수정

--- src/functions/relativeTimeCalculator.js
+++ src/functions/relativeTimeCalculator.js
@@ -3,26 +3,26 @@ export function relativeTimeCalculator(time) {
   const currentTime = new Date(Date.now());
 
   const difference = currentTime - baseTime;
-  const MILlISECOND = 1000;
+  const MILLISECOND = 1000;
   const SECOND = 60;
   const MINUTE = 60;
   const HOUR = 24;
   const DAY = 7;
-  if (difference <= MILlISECOND || difference < 0) {
+  if (difference <= MILLISECOND || difference < 0) {
     return '지금';
-  } else if (difference <= MILlISECOND * SECOND) {
-    return Math.floor(difference / MILlISECOND) + '초전';
-  } else if (difference <= MILlISECOND * SECOND * MINUTE) {
-    return Math.floor(difference / (MILlISECOND * SECOND)) + '분전';
-  } else if (difference <= MILlISECOND * SECOND * MINUTE * HOUR) {
-    return Math.floor(difference / (MILlISECOND * SECOND * MINUTE)) + '시간전';
-  } else if (difference <= MILlISECOND * SECOND * MINUTE * HOUR * DAY) {
+  } else if (difference <= MILLISECOND * SECOND) {
+    return Math.floor(difference / MILLISECOND) + '초전';
+  } else if (difference <= MILLISECOND * SECOND * MINUTE) {
+    return Math.floor(difference / (MILLISECOND * SECOND)) + '분전';
+  } else if (difference <= MILLISECOND * SECOND * MINUTE * HOUR) {
+    return Math.floor(difference / (MILLISECOND * SECOND * MINUTE)) + '시간전';
+  } else if (difference <= MILLISECOND * SECOND * MINUTE * HOUR * DAY) {
     return (
-      Math.floor(difference / (MILlISECOND * SECOND * MINUTE * HOUR)) + '일전'
+      Math.floor(difference / (MILLISECOND * SECOND * MINUTE * HOUR)) + '일전'
     );
   } else {
     return (
-      Math.floor(difference / (MILlISECOND * SECOND * MINUTE * HOUR * DAY)) +
+      Math.floor(difference / (MILLISECOND * SECOND * MINUTE * HOUR * DAY)) +
       '주전'
     );
   }
