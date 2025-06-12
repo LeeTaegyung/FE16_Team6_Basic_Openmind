efff68ae89d3beee7ae6d65252a13c650fb3fc06
tasi <taegyung.e@gmail.com>
2025-06-12 16:36:44 +0900

    refactor(KakaoShareButton): Kakao api 스크립트 추가를 하지 않았을 경우의 방어코드 추가

--- src/pages/PostPage/components/KakaoShareButton.jsx
+++ src/pages/PostPage/components/KakaoShareButton.jsx
@@ -1,15 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import IconKakaoShare from '@assets/icons/IconKakaoShare.svg?react';
 
 import { useGetUser } from '../../../context/UserContext';
 
-const { Kakao } = window;
-
 function KakaoShareButton() {
   const KAKAO_APP_KEY = import.meta.env.VITE_KAKAO_APP_KEY;
+  const KAKAO_APP_URL = import.meta.env.VITE_KAKAO_APP_URL;
+  const KAKAO_APP_INTEGRITY = import.meta.env.VITE_KAKAO_APP_INTEGRITY;
   const { user } = useGetUser();
   const { name, subjectId } = user;
+  const [Kakao, setKakao] = useState(null);
+
+  useEffect(() => {
+    if (window.Kakao) {
+      setKakao(window.Kakao);
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.src = KAKAO_APP_URL;
+    script.integrity = KAKAO_APP_INTEGRITY;
+    script.crossOrigin = 'anonymous';
+
+    script.onload = () => {
+      if (window.Kakao) {
+        setKakao(window.Kakao);
+      }
+    };
+
+    document.head.appendChild(script);
+  }, []);
+
+  useEffect(() => {
+    if (!Kakao) return;
+    Kakao.cleanup();
+    Kakao.init(KAKAO_APP_KEY);
+  }, [Kakao]);
 
   const handleKakaoShare = () => {
     Kakao.Share.sendCustom({
@@ -20,11 +47,6 @@ function KakaoShareButton() {
       },
     });
   };
-
-  useEffect(() => {
-    Kakao.cleanup();
-    Kakao.init(KAKAO_APP_KEY);
-  }, []);
 
   return (
     <button onClick={handleKakaoShare}>
